3b76287d2832c151c111747c625df4f465c3d8e3
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-20 14:04:03 +0200

    feat: add class-validator and class-transformer for input validation in task creation

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
