e56083aa8a7779d38c24ef933a432153080aa333
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-20 10:09:56 +0200

    feat: implement task creation and retrieval in TasksService and TasksController

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksServise: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksServise.getAllTasks();
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
+import { TaskStatus } from 'src/enums/task.enum';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      // id: Date.now().toString(36) + Math.random().toString().slice(2),
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+}
