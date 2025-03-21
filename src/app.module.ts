18f6826af8670276240a6536a563efed5c1aa0b1
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-21 16:51:23 +0200

    feat: integrate JWT authentication with Passport and Config modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './tasks/task.entity';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,27 @@ import { AuthController } from './auth.controller';
 import { UserRepository } from './users.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { TokenProvider } from 'src/config/providers/token.provider';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET_KEY'),
+        signOptions: {
+          expiresIn: '1h', // also can be number in seconds like 3600
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, UserRepository],
+  providers: [AuthService, UserRepository, TokenProvider],
 })
 export class AuthModule {}
