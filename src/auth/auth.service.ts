ec1092c49c9aeb6877190e5c3684245ae2c33479
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-21 16:51:49 +0200

    feat: update signIn method to return access token object and integrate JWT service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,16 +3,23 @@ import { UserRepository } from './users.repository';
 import { AuthCredentialsDto } from './dto/auth.credentials.dto';
 import errorConstants from 'src/constants/error.constants';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
     const user = await this.userRepository.findUserByUsername(username);
 
@@ -26,6 +33,10 @@ export class AuthService {
       throw new UnauthorizedException(errorConstants.INVALID_CREDENTIALS);
     }
 
-    return 'token';
+    const payload: JwtPayload = { id: user.id, username: user.username };
+
+    const accessToken = this.jwtService.sign(payload);
+
+    return { accessToken };
   }
 }
