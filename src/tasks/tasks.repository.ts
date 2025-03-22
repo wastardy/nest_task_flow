3f2fcc1d9bd06ec67ebf0b0d97e58f6634b966c8
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-22 13:36:44 +0200

    feat: modify createTask method to associate task with user

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -5,6 +5,7 @@ import { TaskStatus } from 'src/enums/task-status.enum';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { InternalServerErrorException } from '@nestjs/common';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class TaskRepository extends Repository<Task> {
@@ -46,13 +47,14 @@ export class TaskRepository extends Repository<Task> {
     return foundTask;
   }
 
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
     const { title, description } = createTaskDto;
 
     const task = this.create({
       title,
       description,
       status: TaskStatus.OPEN,
+      user,
     });
 
     try {
