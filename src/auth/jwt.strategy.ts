bba43196a526f1e2ee07858af38da7373ef7e28e
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-22 08:14:29 +0200

    feat: implement JWT strategy for user validation in authentication

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,32 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserRepository } from './users.repository';
+import { TokenProvider } from 'src/config/providers/token.provider';
+import { JwtPayload } from './jwt-payload.interface';
+import { User } from './user.entity';
+import errorConstants from 'src/constants/error.constants';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private userRepository: UserRepository,
+    private tokenProvider: TokenProvider,
+  ) {
+    super({
+      secretOrKey: tokenProvider.getJwtSecretKey(),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { id, username } = payload;
+    const user = await this.userRepository.findUserByUsername(username);
+
+    if (!user) {
+      throw new UnauthorizedException(errorConstants.USER_NOT_FOUND);
+    }
+
+    return user;
+  }
+}
