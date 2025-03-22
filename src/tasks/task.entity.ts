7dc4184df0a0c99fc75c7bd254bb2c5d5559bfef
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-22 13:41:22 +0200

    feat: exclude user property from plain object transformation in Task entity

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude } from 'class-transformer';
 import { User } from 'src/auth/user.entity';
 import { TaskStatus } from 'src/enums/task-status.enum';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
@@ -17,5 +18,6 @@ export class Task {
   status: TaskStatus;
 
   @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  @Exclude({ toPlainOnly: true })
   user: User;
 }
