1051a428e657fee99bf9f7a6e54a9e564907b48c
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-22 13:19:29 +0200

    feat: establish one-to-many relationship between User and Task entities

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,5 +1,6 @@
+import { User } from 'src/auth/user.entity';
 import { TaskStatus } from 'src/enums/task-status.enum';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Task {
@@ -14,4 +15,7 @@ export class Task {
 
   @Column()
   status: TaskStatus;
+
+  @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  user: User;
 }
