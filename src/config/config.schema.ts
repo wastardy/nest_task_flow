f93e86462ebbdcc3cd9e360f7e90169bfb75b6ec
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-24 01:11:54 +0200

    feat: update config validation schema to include BASE_URL, HOST, PORT, and JWT_SECRET_KEY with default values

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -1,10 +1,17 @@
 import * as Joi from '@hapi/joi';
 
 export const configValidationsSchema = Joi.object({
-  // STAGE: Joi.string().required(),
+  BASE_URL: Joi.string().default('http://localhost:3000').required(),
+  HOST: Joi.string().default('localhost').required(),
+  PORT: Joi.number().default(3000).required(),
+
+  // DB Configurations
   DB_HOST: Joi.string().default('localhost').required(),
   DB_PORT: Joi.number().default(5432).required(),
   DB_USER: Joi.string().default('postgres').required(),
   DB_PASSWORD: Joi.string().default('postgres').required(),
   DB_NAME: Joi.string().default('task-management').required(),
+
+  // JWT Secret Key
+  JWT_SECRET_KEY: Joi.string().required(),
 });
